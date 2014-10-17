43ac158d7046c5469620507497f9a46894530f18
Scott Liu <liuleeeeee@icloud.com>
2014-10-17 20:23:51 +0800

    Create Mama_I_love_you.c

---
+++ Mama_I_love_you.c
@@ -0,0 +1,12 @@
+/*  sum_squ.c  */
+#include <stdio.h>
+int main(void)
+{
+  int toes,sum,squ;
+  toes=10;
+  sum=toes+toes;
+  squ=toes*toes;
+  printf("toes=%d;\nsum=%d;\nsqu=%d.\n",toes,sum,squ);
+  getch();
+  return0;
+}
